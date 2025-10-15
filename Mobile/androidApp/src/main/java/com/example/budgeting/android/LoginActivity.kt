8d033099a12b2a1271c7aa26144c9d4d6058932c
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-10-15 19:15:11 +0300

    [FEAT] implemented the navigation between login and registration pages

--- Mobile/androidApp/src/main/java/com/example/budgeting/android/LoginActivity.kt
+++ Mobile/androidApp/src/main/java/com/example/budgeting/android/LoginActivity.kt
@@ -3,29 +3,33 @@ package com.example.budgeting.android
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.MaterialTheme
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
 import com.example.budgeting.android.ui.screens.LoginScreen
 
-
-class LoginActivity : ComponentActivity(){
+class LoginActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MaterialTheme {
-                LoginScreen { username, password ->
-                    // TODO - check if the username and password are correct before login
+                LoginScreen(
+                    onLoginClick = { username, password ->
+                        // TODO - check if the username and password are correct before login
 
-                    val intent = Intent(this, MainActivity::class.java)
-                    intent.putExtra("USERNAME", username)
-                    startActivity(intent)
-                    // finish the activity so the user can't go back to the login screen
-                    finish()
-                }
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        intent.putExtra("USERNAME", username)
+                        startActivity(intent)
+                        // finish the activity so the user can't go back to the login screen
+                        finish()
+                    },
+                    onSignUpClick = {
+                        val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                        startActivity(intent)
+                    }
+                )
             }
         }
-
     }
 }
