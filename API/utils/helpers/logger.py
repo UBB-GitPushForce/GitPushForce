c4b0269ea235a8a6e8595d731c58cab69d7ce412
Robert076 <robertberes06@gmail.com>
2025-11-10 11:08:40 +0200

    bugfix: revert logger

--- API/utils/helpers/logger.py
+++ API/utils/helpers/logger.py
@@ -6,7 +6,7 @@ import threading
 class Logger:
     _instance = None
     _lock = threading.Lock()
-
+    
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             with cls._lock:
@@ -22,50 +22,23 @@ class Logger:
 
         if not self.logger.handlers:
             formatter = logging.Formatter(
-                fmt='[%(levelname)s] [%(caller)s] %(message)s'
+                fmt='[%(levelname)s] [%(funcName)s] %(message)s'
             )
             console_handler = logging.StreamHandler()
             console_handler.setFormatter(formatter)
             self.logger.addHandler(console_handler)
 
-    def _log(self, level, msg, *args, **kwargs):
-        # mergem înapoi în stack până găsim metoda reală (cu self)
-        frame = inspect.currentframe()
-        caller = None
-
-        for _ in range(6):  # limită de siguranță (evită buclă infinită)
-            frame = frame.f_back
-            if not frame:
-                break
-
-            func_name = frame.f_code.co_name
-            module_name = frame.f_globals.get('__name__', '<module>')
-            if "self" in frame.f_locals:
-                cls_name = frame.f_locals["self"].__class__.__name__
-                caller = f"{module_name}.{cls_name}.{func_name}"
-                break
-
-        # fallback dacă nu găsește nicio clasă
-        if not caller:
-            func_name = frame.f_code.co_name if frame else "<unknown>"
-            module_name = frame.f_globals.get('__name__', '<module>') if frame else "<module>"
-            caller = f"{module_name}.{func_name}"
-
-        extra = {"caller": caller}
-        self.logger.log(level, msg, *args, extra=extra, stacklevel=3, **kwargs)
-
-    # metode standard
     def debug(self, msg, *args, **kwargs):
-        self._log(logging.DEBUG, msg, *args, **kwargs)
+        self.logger.debug(msg, *args, stacklevel=2, **kwargs)
 
     def info(self, msg, *args, **kwargs):
-        self._log(logging.INFO, msg, *args, **kwargs)
+        self.logger.info(msg, *args, stacklevel=2, **kwargs)
 
     def warning(self, msg, *args, **kwargs):
-        self._log(logging.WARNING, msg, *args, **kwargs)
+        self.logger.warning(msg, *args, stacklevel=2, **kwargs)
 
     def error(self, msg, *args, **kwargs):
-        self._log(logging.ERROR, msg, *args, **kwargs)
+        self.logger.error(msg, *args, stacklevel=2, **kwargs)
 
     def critical(self, msg, *args, **kwargs):
-        self._log(logging.CRITICAL, msg, *args, **kwargs)
+        self.logger.critical(msg, *args, stacklevel=2, **kwargs)
