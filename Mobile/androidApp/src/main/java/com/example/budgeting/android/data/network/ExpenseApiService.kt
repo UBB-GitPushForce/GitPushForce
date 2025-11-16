0653ec2eb237a57f447127b85593399440a41e8f
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-11-17 00:25:38 +0200

    Update ExpenseApiService.kt

--- Mobile/androidApp/src/main/java/com/example/budgeting/android/data/network/ExpenseApiService.kt
+++ Mobile/androidApp/src/main/java/com/example/budgeting/android/data/network/ExpenseApiService.kt
@@ -8,6 +8,7 @@ import retrofit2.http.DELETE
 import com.example.budgeting.android.data.model.Expense
 import retrofit2.http.Body
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ExpenseApiService {
 
@@ -22,5 +23,4 @@ interface ExpenseApiService {
 
     @DELETE("/expenses/{id}")
     suspend fun deleteExpense(@Path("id") id: Int): Response<String>
-
 }
